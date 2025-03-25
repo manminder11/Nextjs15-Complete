7e39c6aeb65b2d8361b7eb65e9af0dc594f2371f
manminder11 <ManminderSingh@edu.sait.ca>
2025-03-25 09:52:26 -0600

    Routing and nested routes

---
+++ src/app/users/info/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>information of the users</div>;
+};
+
+export default page;
---
+++ src/app/users/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>you are in the users page</div>;
+};
+
+export default page;
