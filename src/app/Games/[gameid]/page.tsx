cba0a8e7449582d280dd50ffd10a55954924eb73
manminder11 <ManminderSingh@edu.sait.ca>
2025-03-25 13:37:34 -0600

    Dynamic routes

---
+++ src/app/Games/[gameid]/page.tsx
@@ -0,0 +1,11 @@
+const G = async ({ params }: { params: { gameid: string } }) => {
+  const { gameid } = await params;
+  console.log(gameid);
+
+  return <h1>my fav game is {gameid}</h1>;
+};
+
+// // Example usage of the G function
+//G({ params: { gameid: "example-game-id" } });
+
+export default G;
