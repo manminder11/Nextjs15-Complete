a83d58de57aa54626522b556c699be1574b5e96f
manminder11 <ManminderSingh@edu.sait.ca>
2025-03-25 13:12:57 -0600

    Private routes

---
+++ src/app/auth/PR/page.tsx
@@ -0,0 +1,14 @@
+const page = () => {
+  return (
+    <div>
+this is a private page       
+    </div>
+  )
+}
+
+export default page
+
+
+// auth is the folder which is  provoking the straight acess
+// by ()puttting the auth in the parenthesis we can have a straight acess to the page (auth)
+// mysteriously my (auth) is not visible after putting (auth) in the parenthesis
