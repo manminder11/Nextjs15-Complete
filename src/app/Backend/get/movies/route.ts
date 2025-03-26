ee25c6b6ab8c392de9f87a6fddbaa2294a3de9b7
manminder11 <ManminderSingh@edu.sait.ca>
2025-03-26 10:59:48 -0600

    POST method coverd

--- src/app/Backend/get/movies/route.ts
+++ src/app/Backend/get/movies/route.ts
@@ -5,3 +5,11 @@ import { movies } from "./db";
 export async function GET() {
   return Response.json(movies);
 }
+
+export async function POST(req: Request) {
+  const movie = await req.json();
+
+  const Newmovie = { ...movie };
+  movies.push(Newmovie);
+  return new Response(JSON.stringify(Newmovie));
+}
