cc0b14d1cf10faf1c9d4cd86b806e09b1b239d21
manminder11 <ManminderSingh@edu.sait.ca>
2025-03-26 09:01:56 -0600

    not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+const NF = () => {
+  return (
+    <div>
+      <h2 className="text-3xl text-center text-red-600">
+        Sorry this page cannot be found because i dont want that you access this{" "}
+      </h2>
+    </div>
+  );
+};
+
+export default NF;
